efe5dedf5ce9a39968fc1efa5f05d607f9cdfd79
pdasigi <pradeepd@allenai.org>
2019-09-20 17:01:40 -0700

    fixed dataset reader

--- gensem/readers/dataset_reader.py
+++ gensem/readers/dataset_reader.py
@@ -2,6 +2,7 @@ import logging
 from typing import Dict, List
 import os
 import json
+import gzip
 
 from overrides import overrides
 
@@ -10,6 +11,7 @@ from allennlp.data.instance import Instance
 from allennlp.data.fields import (Field, TextField, MetadataField, ProductionRuleField,
                                   ListField)
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
+from allennlp.data.dataset_readers.semantic_parsing.wikitables import util as wtq_data_util
 from allennlp.data.tokenizers import WordTokenizer, Token
 from allennlp.data.tokenizers.tokenizer import Tokenizer
 from allennlp.data.tokenizers.word_splitter import SpacyWordSplitter
@@ -46,30 +48,53 @@ class WikiTablesQuestionGeneratorReader(DatasetReader):
 
     @overrides
     def _read(self, file_path: str):
-        # TODO(pradeep): Fix.
-        if self._offline_logical_forms_directory is not None:
-            logger.warning("Cannot handle multiple logical forms per instance yet!")
+        data: List[Dict[str, str]] = []
+        if file_path.endswith(".jsonl"):
+            with open(file_path, "r") as data_file:
+                for line in data_file:
+                    if not line:
+                        continue
+                    line_data = json.loads(line)
+                    line_data["logical_form"] = [line_data["logical_form"]]
+                    data.append(line_data)
+        elif file_path.endswith(".examples"):
+            num_examples = 0
+            num_examples_without_lf = 0
+            if self._offline_logical_forms_directory is None:
+                raise RuntimeError("Logical forms directory required when processing examples files!")
+            with open(file_path, "r") as data_file:
+                for line in data_file:
+                    num_examples += 1
+                    line_data = wtq_data_util.parse_example_line(line)
+                    example_id = line_data["id"]
+                    logical_forms_file = os.path.join(self._offline_logical_forms_directory,
+                                                      f"{example_id}.gz")
+                    if not os.path.exists(logical_forms_file):
+                        num_examples_without_lf += 1
+                        continue
+                    logical_forms = None
+                    with gzip.open(logical_forms_file, "rt") as lf_file:
+                        logical_forms = [x.strip() for x in lf_file.readlines()]
+                    line_data["logical_form"] = logical_forms
+                    data.append(line_data)
+            logger.info(f"Skipped {num_examples_without_lf} out of {num_examples} examples")
+        else:
+            raise RuntimeError(f"Unknown file type: {file_path}. Was expecting either *.examples or *.jsonl")
 
-        with open(file_path, "r") as data_file:
-            for line in data_file:
-                if not line:
-                    continue
-                line_data = json.loads(line)
-                question = line_data["question"]
-                logical_form = line_data["logical_form"]
-                # We want the tagged file, but the ``*.examples`` files typically point to CSV.
-                table_filename = os.path.join(self._tables_directory,
-                                              line_data["table_filename"].replace("csv", "tagged"))
+        for datum in data:
+            # We want the tagged file, but the ``*.examples`` files typically point to CSV.
+            table_filename = os.path.join(self._tables_directory,
+                                          datum["table_filename"].replace("csv", "tagged"))
 
-                table_lines = [line.split("\t") for line in open(table_filename).readlines()]
-                instance = self.text_to_instance(logical_form=logical_form,
-                                                 table_lines=table_lines,
-                                                 question=question)
-                if instance is not None:
-                    yield instance
+            table_lines = [line.split("\t") for line in open(table_filename).readlines()]
+            instance = self.text_to_instance(logical_forms=datum["logical_form"],
+                                             table_lines=table_lines,
+                                             question=datum["question"])
+            if instance is not None:
+                yield instance
 
     def text_to_instance(self,  # type: ignore
-                         logical_form: str,
+                         logical_forms: List[str],
                          table_lines: List[List[str]],
                          question: str) -> Instance:
         # pylint: disable=arguments-differ
@@ -80,41 +105,46 @@ class WikiTablesQuestionGeneratorReader(DatasetReader):
         table_context = TableQuestionContext.read_from_lines(table_lines, tokenized_question)
         world = WikiTablesLanguage(table_context)
 
-        action_sequence = None
-        try:
-            action_sequence = world.logical_form_to_action_sequence(logical_form)
-            action_sequence = reader_utils.make_bottom_up_action_sequence(action_sequence,
-                                                                          world.is_nonterminal)
-            action_sequence_field = TextField([Token(rule) for rule in  action_sequence],
-                                              self._rule_indexers)
-        except ParsingError as error:
-            logger.debug(f'Parsing error: {error.message}, skipping logical form')
-            logger.debug(f'Question was: {question}')
-            logger.debug(f'Logical form was: {logical_form}')
-            logger.debug(f'Table info was: {table_lines}')
-        except:
-            logger.error(logical_form)
-            raise
+        action_sequences_list: List[List[str]] = []
+        action_sequence_fields_list: List[TextField] = []
+        for logical_form in logical_forms:
+            try:
+                action_sequence = world.logical_form_to_action_sequence(logical_form)
+                action_sequence = reader_utils.make_bottom_up_action_sequence(action_sequence,
+                                                                              world.is_nonterminal)
+                action_sequence_field = TextField([Token(rule) for rule in  action_sequence],
+                                                  self._rule_indexers)
+                action_sequences_list.append(action_sequence)
+                action_sequence_fields_list.append(action_sequence_field)
+            except ParsingError as error:
+                logger.debug(f'Parsing error: {error.message}, skipping logical form')
+                logger.debug(f'Question was: {question}')
+                logger.debug(f'Logical form was: {logical_form}')
+                logger.debug(f'Table info was: {table_lines}')
+            except:
+                logger.error(logical_form)
+                raise
 
-        if not action_sequence:
+        if not action_sequences_list:
             return None
 
-        # Not very happy about calling the action sequence field "source_tokens", but doing so to make these
-        # instances compatible with SimpleSeq2Seq.
-        fields = {'source_tokens': action_sequence_field,
+        fields = {'action_sequences': ListField(action_sequence_fields_list),
                   'target_tokens': question_field}
 
         if self._output_world:
             fields['world'] = MetadataField(world)
 
         if self._output_production_rules:
-            production_rule_fields: List[Field] = []
-            for production_rule in action_sequence:
-                _, rule_right_side = production_rule.split(' -> ')
-                is_global_rule = not world.is_instance_specific_entity(rule_right_side)
-                field = ProductionRuleField(production_rule, is_global_rule=is_global_rule)
-                production_rule_fields.append(field)
-            action_field = ListField(production_rule_fields)
+            all_production_rule_fields: List[List[Field]] = []
+            for action_sequence in action_sequences_list:
+                all_production_rule_fields.append([])
+                for production_rule in action_sequence:
+                    _, rule_right_side = production_rule.split(' -> ')
+                    is_global_rule = not world.is_instance_specific_entity(rule_right_side)
+                    field = ProductionRuleField(production_rule, is_global_rule=is_global_rule)
+                    all_production_rule_fields[-1].append(field)
+            action_field = ListField([ListField(production_rule_fields) for production_rule_fields in
+                                      all_production_rule_fields])
             fields['actions'] = action_field
 
         return Instance(fields)
--- tests/readers/dataset_reader_test.py
+++ tests/readers/dataset_reader_test.py
@@ -8,7 +8,7 @@ from allennlp.semparse.domain_languages import WikiTablesLanguage
 from gensem.readers.dataset_reader import WikiTablesQuestionGeneratorReader
 
 class TestWikiTablesBackTranslationDatasetReader(AllenNlpTestCase):
-    def test_reader_reads(self):
+    def test_reader_reads_jsonl(self):
         params = {
                 'lazy': False,
                 'tables_directory': "fixtures/data/tables",
@@ -20,7 +20,7 @@ class TestWikiTablesBackTranslationDatasetReader(AllenNlpTestCase):
         instance = instances[0]
 
         assert instance.fields.keys() == {
-                'source_tokens',
+                'action_sequences',
                 'target_tokens',
                 'world',
                 'actions',
@@ -34,20 +34,50 @@ class TestWikiTablesBackTranslationDatasetReader(AllenNlpTestCase):
         # sure we get a WikiTablesWorld object in here.
         assert isinstance(instance.fields['world'].as_tensor({}), WikiTablesLanguage)
 
-        action_fields = instance.fields['actions'].field_list
-        actions = [action_field.rule for action_field in action_fields]
+        all_action_fields = instance.fields['actions'].field_list
+        actions = [[action_field.rule for action_field in action_fields] for action_fields in all_action_fields]
 
         # Actions in bottom-up order.
-        expected_sequence = ['<List[Row],StringColumn:List[str]> -> select_string',
-                             '<List[Row]:List[Row]> -> previous',
-                             '<List[Row],StringColumn,List[str]:List[Row]> -> filter_in',
-                             'List[Row] -> all_rows',
-                             'StringColumn -> string_column:incumbent',
-                             'List[str] -> string:h_w_whillock',
-                             'List[Row] -> [<List[Row],StringColumn,List[str]:List[Row]>, List[Row], StringColumn, List[str]]',  # pylint: disable=line-too-long
-                             'List[Row] -> [<List[Row]:List[Row]>, List[Row]]',
-                             'StringColumn -> string_column:incumbent',
-                             'List[str] -> [<List[Row],StringColumn:List[str]>, List[Row], StringColumn]',
-                             '@start@ -> List[str]']
+        expected_sequence = [['<List[Row],StringColumn:List[str]> -> select_string',
+                              '<List[Row]:List[Row]> -> previous',
+                              '<List[Row],StringColumn,List[str]:List[Row]> -> filter_in',
+                              'List[Row] -> all_rows',
+                              'StringColumn -> string_column:incumbent',
+                              'List[str] -> string:h_w_whillock',
+                              'List[Row] -> [<List[Row],StringColumn,List[str]:List[Row]>, List[Row], StringColumn, List[str]]',  # pylint: disable=line-too-long
+                              'List[Row] -> [<List[Row]:List[Row]>, List[Row]]',
+                              'StringColumn -> string_column:incumbent',
+                              'List[str] -> [<List[Row],StringColumn:List[str]>, List[Row], StringColumn]',
+                              '@start@ -> List[str]']]
         assert actions == expected_sequence
-        assert [t.text for t in instance.fields["source_tokens"].tokens] == expected_sequence
+        assert [[t.text for t in text_field.tokens]
+                for text_field in instance.fields["action_sequences"]] == expected_sequence
+
+    def test_reader_reads_examples(self):
+        params = {
+                'lazy': False,
+                'tables_directory': "fixtures/data/tables",
+                'offline_logical_forms_directory': "fixtures/data/logical_forms"
+                }
+        reader = WikiTablesQuestionGeneratorReader.from_params(Params(params))
+        dataset = reader.read("fixtures/data/sample_data.examples")
+        instances = list(dataset)
+        assert len(instances) == 2
+        instance = instances[0]
+
+        assert instance.fields.keys() == {
+                'action_sequences',
+                'target_tokens',
+                'world',
+                'actions',
+                }
+
+        question_tokens = [START_SYMBOL, "who", "was", "appointed", "before", "h.w", ".", "whillock", "?",
+                           END_SYMBOL]
+        assert [t.text for t in instance.fields["target_tokens"].tokens] == question_tokens
+
+        # The content of this will be tested indirectly by checking the actions; we'll just make
+        # sure we get a WikiTablesWorld object in here.
+        assert isinstance(instance.fields['world'].as_tensor({}), WikiTablesLanguage)
+        all_action_fields = instance.fields['actions'].field_list
+        assert len(all_action_fields) == 2
